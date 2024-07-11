c846061db2189a5089cea6575fb65edbf2ce6926
Seoyoung Kim <kim@d207-023-197-046.wireless.sfu.ca>
2024-07-10 17:30:58 -0700

    Still

--- Product.cpp
+++ Product.cpp
@@ -22,6 +22,46 @@ void Product::DisplayDetails(ostream &out) const{
     }
 }   
 
+int ValidateProduct(const int &ReleaseID, const char *ReleaseDate){
+    string date = string(ReleaseDate);
+    //01 2 34 5 67
+    string yy, mm, dd;
+    yy = date.substr(0, 2);
+    mm = date.substr(3, 5);
+    dd = date.substr(6, 8);
+
+    int year, month, day;
+    year = stoi(yy);
+    month = stoi(mm);
+    day = stoi(dd);
+
+    if (year < 24 || year > 99 || month < 1 || month > 12 || day < 1 || day > 31){
+        return -1;
+    }
+    else {
+        if (month == 2){
+            if (year % 4 == 0 && day > 29) return -1;
+            else if (day > 28) return -1;
+        }
+        //2,4,6,9,11
+        if (day == 31){
+            switch(month){
+                case 4:
+                    return -1;
+                case 6:
+                    return -1;
+                case 9:
+                    return -1;
+                case 11:
+                    return -1;
+            };
+        }
+    }
+
+    return 1;
+
+}
+
 Product CreateProduct(const int &ReleaseID, const char *ReleaseDate){
     if (ValidateProduct(ReleaseID, ReleaseDate)){
         Product newProd(ReleaseDate);
